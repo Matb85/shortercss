4192917a67e014425b5eae47522c9c1794b184a9
Mateusz BIS <Mateuszbis85@gmail.com>
2021-01-29 12:22:06 +0100

    feat!: change typescript target to ES6

--- dist/index.js
+++ dist/index.js
@@ -2,54 +2,20 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var library_1 = __importDefault(require("./utils/library"));
-var lodash_1 = require("lodash");
-var event_stream_1 = require("event-stream");
-var gulp_util_1 = __importDefault(require("gulp-util"));
-var app_root_path_1 = __importDefault(require("app-root-path"));
-var Plugin = (function () {
-    function Plugin(config) {
+Object.defineProperty(exports, "__esModule", { value: true });
+const library_1 = __importDefault(require("./utils/library"));
+const lodash_1 = __importDefault(require("lodash"));
+const app_root_path_1 = __importDefault(require("app-root-path"));
+module.exports = class CssTerser {
+    constructor(config) {
         if (typeof config === "undefined")
-            config = require(app_root_path_1["default"] + "/cssterser.config.js");
+            config = require(app_root_path_1.default + "/cssterser.config.js");
         if (typeof config === "string")
-            config = require(app_root_path_1["default"] + config);
-        this.ignores = lodash_1.extend({ classes: [], ids: [] }, config.ignores);
-        this.bindings = lodash_1.extend({ css: ["css"], html: ["html"] }, config.bindings);
+            config = require(app_root_path_1.default + config);
+        this.ignores = lodash_1.default.extend({ classes: [], ids: [] }, config.ignores);
+        this.bindings = lodash_1.default.extend({ css: ["css"], html: ["html"] }, config.bindings);
         this.processors = config.processors;
-        this.classLibrary = new library_1["default"](this.ignores.classes);
-        this.idLibrary = new library_1["default"](this.ignores.ids);
-    }
-    Plugin.prototype.run = function () {
-        var _this = this;
-        var miniSelectors = function (file, callback) {
-            var extensions = file.path.split(".");
-            var extension = extensions[extensions.length - 1];
-            var reducedFile = String(file.contents);
-            for (var binding in _this.bindings) {
-                if (_this.bindings[binding].includes(extension)) {
-                    reducedFile = _this.processors[binding](reducedFile, _this.classLibrary, _this.idLibrary);
-                }
-            }
-            file.contents = Buffer.from(reducedFile);
-            callback(null, file);
-        };
-        return event_stream_1.map(miniSelectors);
-    };
-    Plugin.prototype.info = function () {
-        var _this = this;
-        return event_stream_1.map(function (file, callback) {
-            gulp_util_1["default"].log(file.history[0]);
-            gulp_util_1["default"].log("Class library:", _this.classLibrary.stats());
-            gulp_util_1["default"].log("ID library:", _this.idLibrary.stats());
-            callback(null, file);
-        });
-    };
-    return Plugin;
-}());
-module.exports = {
-    Plugin: Plugin,
-    init: function (config) {
-        return new Plugin(config);
+        this.classLibrary = new library_1.default(this.ignores.classes);
+        this.idLibrary = new library_1.default(this.ignores.ids);
     }
 };
--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,11 @@
 import { IgnoresType, Processors, Bindings, Config } from "./src/index";
 import { LibraryInstance } from "./src/utils/library";
 
-export class CssTerser {
+export default class CssTerser {
   ignores: IgnoresType;
   processors: Processors;
   bindings: Bindings;
   classLibrary: LibraryInstance;
   idLibrary: LibraryInstance;
-  run();
   constructor(config: Config);
 }
-
-export function init(config: Config): CssTerser;
